06f6427a328264583094da6f9149b04d15a064dc
SanjithMohanan <sanjith.mohanan@gmail.com>
2020-02-09 15:17:00 -0600

    initial commit for DevOps project

---
+++ src/test/java/devopspackage/HelloClassTest.java
@@ -0,0 +1,14 @@
+package devopspackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloClassTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
